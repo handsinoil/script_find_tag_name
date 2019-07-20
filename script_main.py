a426d94972c84016b852f40fd0d72ceac12d92f2
handsinoil <handsinoil@gmail.com>
2019-07-20 22:20:50 +0400

    Написал список задач для выполнения

---
+++ script_main.py
@@ -0,0 +1,12 @@
+#! python
+import sys, os
+# TODO 1. input_tag_and_directory( принимает тег для поиска, директорию, где искать, тег для поиска внутри файла)
+# TODO 2. search_files(directory_search_tag, directory_path)
+# TODO 3. file_select
+# TODO 4. open_in_file
+
+if __name__ == "__main__":
+    input_tag_and_directory()
+    search_files()
+    file_select()
+    open_in_file()
