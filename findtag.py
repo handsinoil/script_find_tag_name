ca8e550e3aac93907cd65283da78bdddbfa2d45f
handsinoil <handsinoil@gmail.com>
2019-07-21 11:08:18 +0400

    поправлено оформление

--- findtag.py
+++ findtag.py
@@ -1,15 +1,16 @@
-#! python3
-import sys, os
+#!/usr/bin/env python3
+import os
+import sys
 
 
 # TODO 1. input_tag_and_directory( принимает тег для поиска, директорию, где искать, тег для поиска внутри файла)
 def input_tag_and_directory():
-    #arguments_for_search = sys.argv
+    # arguments_for_search = sys.argv
     if len(sys.argv) < 2:
         print('Использование для Linux: python3 findtag.py [имя тега] [адрес директории поиска, '
-              'по умолчанию ищет в текущей]'+'\n'+'Использование для Windows: py findtag.py [имя тега] '
-              '[адрес директории поиска, '
-              'по умолчанию ищет в текущей]')
+              'по умолчанию ищет в текущей]' + '\n' +
+              'Использование для Windows: py findtag.py [имя тега] '
+              '[адрес директории поиска, по умолчанию ищет в текущей]')
         sys.exit()
     elif len(sys.argv) < 3:
         print('Для поиска будет использована текущая директория')
@@ -26,18 +27,19 @@ def search_files(directory_search_tag, directory_path):
     for file in file_list:
         file_path = os.path.join(directory_path, file)
         if os.path.isfile(file_path) and directory_search_tag in file:
-                result_file_list.append(file)
+            result_file_list.append(file)
     print('Найдены следующие файлы:')
     for i in range(len(result_file_list)):
         print(f'{i}. {result_file_list[i]}')
     return sorted(result_file_list)
 
+
 # TODO 3. file_select
 # TODO 4. open_in_file
 
+def main() -> None:
+    search_files(input_tag_and_directory)
+    
 
 if __name__ == "__main__":
-    #input_tag_and_directory()
-    search_files(input_tag_and_directory)
-    file_select()
-    open_in_file()
+    main()
